249679fa456d0c8fb1461e7c16a37868f4ed854a
J Fixby <github@jfixby.com>
2015-11-11 13:32:28 +0100

    +producePackageDescriptor

---
+++ rana-asset-red/rana-asset-red/com/jfixby/red/engine/core/resources/PackageUtils.java
@@ -0,0 +1,34 @@
+package com.jfixby.red.engine.core.resources;
+
+import java.io.IOException;
+
+import com.jfixby.cmns.api.assets.AssetID;
+import com.jfixby.cmns.api.collections.Collection;
+import com.jfixby.cmns.api.filesystem.File;
+import com.jfixby.cmns.api.json.Json;
+import com.jfixby.cmns.api.sys.Sys;
+import com.jfixby.rana.api.pkg.fs.PackageDescriptor;
+
+public class PackageUtils {
+
+	static public void producePackageDescriptor(File package_folder, String format, String version, Collection<AssetID> provisions, Collection<AssetID> dependencies, String root_file_name) throws IOException {
+
+		PackageDescriptor descriptor = new PackageDescriptor();
+		descriptor.format = format;
+		descriptor.timestamp = "" + Sys.SystemTime().currentTimeMillis();
+		descriptor.version = version;
+		for (AssetID d : provisions) {
+			descriptor.packed_assets.addElement(d.toString());
+		}
+		for (AssetID d : dependencies) {
+			descriptor.package_dependencies.addElement(d.toString());
+		}
+
+		descriptor.root_file_name = root_file_name;
+		File output_file = package_folder.child(PackageDescriptor.PACKAGE_DESCRIPTOR_FILE_NAME);
+
+		output_file.writeString(Json.serializeToString(descriptor));
+
+	}
+
+}
